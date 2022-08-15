c458d720ed81a15ba6af45c33613d24d64fa96f8
Alexandra Salas <106403897+vashomeinspection@users.noreply.github.com>
2022-08-14 23:23:53 -0400

    Starting homepage body--- contact button needs fixing

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -3,28 +3,39 @@ import { Container, Button, Col } from "react-bootstrap";
 import HomeBody from "./homeBody";
 import Type from "./Type";
 
-import { useNavigate } from "react-router-dom";
+
 
 
 function Home() {
-  const navigate = useNavigate();
+
   return (
     <section>
+      
       <Container fluid className="home-section" id="home">
+
           <Col className="home-title">
             <h1>VAS HOME INSPECTIONS</h1>
             <h2> <Type /> </h2>
-          </Col>
-          <Button
+            <Button Flex
             variant="primary"
-            href={"/about"}
-            target="_blank"
+            href="/contact-us"
             style={{ maxWidth: "250px" }}
-          >
-            &nbsp;Download CV
+            >
+            Schedule Inspection ---- doesnt work
           </Button>
+          </Col>
+          
       </Container>
-      <HomeBody />
+      <div id="contactButton">
+            <Button
+            variant="primary"
+            href="/contact-us"
+            style={{ maxWidth: "250px" }}
+            >
+            Schedule Inspection -- does work some how
+          </Button>
+          </div>
+    <HomeBody />
     </section>
   );
 }
